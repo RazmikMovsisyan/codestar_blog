989f37a24c9b2f20f50e518f61689f727ee2f1b4
Razmik Movsisyan <razmikmovsisyan@Razmiks-MBP.fritz.box>
2025-05-10 21:37:48 +0200

    Add comment Edit function

--- about/urls.py
+++ about/urls.py
@@ -1,9 +1,7 @@
+
 from . import views
 from django.urls import path
 
 urlpatterns = [
     path('', views.about_me, name='about'),
-    path('<slug:slug>/edit_comment/<int:comment_id>',
-    views.comment_edit, name='comment_edit'),
-
 ]
