9cc66a9c7111442032ef7051d4262b08c82e6e20
Razmik Movsisyan <razmikmovsisyan@Razmiks-MBP.fritz.box>
2025-05-10 21:32:32 +0200

    Add comment Edit function

--- about/views.py
+++ about/views.py
@@ -1,18 +1,53 @@
 from django.shortcuts import render
+from django.contrib import messages
 
 # Create your views here.
 from django.shortcuts import render
 from .models import About
+from .forms import CollaborateForm
+
 
 
 def about_me(request):
     """
     Renders the About page
     """
+    if request.method == "POST":
+        collaborate_form = CollaborateForm(data=request.POST)
+        if collaborate_form.is_valid():
+            collaborate_form.save()
+            messages.add_message(request, messages.SUCCESS, "Collaboration request received! I endeavour to respond within 2 working days.")
     about = About.objects.all().order_by('-updated_on').first()
+    collaborate_form = CollaborateForm()
+
 
     return render(
         request,
         "about/about.html",
-        {"about": about},
+        {
+            "about": about,
+            "collaborate_form": collaborate_form
+        },
     )
+
+def comment_edit(request, slug, comment_id):
+    """
+    view to edit comments
+    """
+    if request.method == "POST":
+
+        queryset = Post.objects.filter(status=1)
+        post = get_object_or_404(queryset, slug=slug)
+        comment = get_object_or_404(Comment, pk=comment_id)
+        comment_form = CommentForm(data=request.POST, instance=comment)
+
+        if comment_form.is_valid() and comment.author == request.user:
+            comment = comment_form.save(commit=False)
+            comment.post = post
+            comment.approved = False
+            comment.save()
+            messages.add_message(request, messages.SUCCESS, 'Comment Updated!')
+        else:
+            messages.add_message(request, messages.ERROR, 'Error updating comment!')
+
+    return HttpResponseRedirect(reverse('post_detail', args=[slug]))
